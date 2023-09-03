f319a0f4df5773c727e0fa8298e83d9458768929
EduardoPicolo <eduardonpicolo@gmail.com>
2023-09-03 05:37:10 -0300

    Fix github actions type-check

---
+++ src/types/image.d.ts
@@ -0,0 +1,19 @@
+declare module '*.webp' {
+	const value: import('next/image').StaticImageData;
+	export default value;
+}
+
+declare module '*.png' {
+	const value: import('next/image').StaticImageData;
+	export default value;
+}
+
+declare module '*.jpg' {
+	const value: import('next/image').StaticImageData;
+	export default value;
+}
+
+declare module '*.svg' {
+	const value: import('next/image').StaticImageData;
+	export default value;
+}
