80583b9249bea68f767e9cec965f48408043a430
EduardoPicolo <eduardonpicolo@gmail.com>
2024-03-23 15:57:36 -0300

    Update eslint

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -5,15 +5,17 @@ const project = resolve(process.cwd(), 'tsconfig.json');
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
 	extends: [
+		require.resolve('@vercel/style-guide/eslint/node'),
+		require.resolve('@vercel/style-guide/eslint/browser'),
+		require.resolve('@vercel/style-guide/eslint/typescript'),
+		require.resolve('@vercel/style-guide/eslint/react'),
+		require.resolve('@vercel/style-guide/eslint/next'),
 		'plugin:eslint-comments/recommended',
-		'plugin:import/typescript',
 		'plugin:react-perf/recommended',
 		'plugin:promise/recommended',
 		'plugin:cypress/recommended',
-
-		'eslint:recommended',
-		'prettier',
-		require.resolve('@vercel/style-guide/eslint/next'),
+		'plugin:prettier/recommended',
+		'plugin:@next/next/recommended',
 		'eslint-config-turbo',
 	],
 	globals: {
@@ -25,13 +27,18 @@ module.exports = {
 	},
 	plugins: [
 		'only-warn',
-		'@typescript-eslint',
+		/**
+		 * NOTE: `@typescript-eslint` plugin should have the same version
+		 * as `@vercel/style-guide/eslint`
+		 */
+		// '@typescript-eslint',
 		'eslint-comments',
 		'promise',
 		'simple-import-sort',
 		'unused-imports',
 		'react-perf',
 		'eslint-plugin-no-inline-styles',
+		'eslint-plugin-tsdoc',
 		'testing-library',
 	],
 	settings: {
@@ -48,6 +55,14 @@ module.exports = {
 		'react-perf/jsx-no-new-object-as-prop': 'warn',
 		'react-perf/jsx-no-new-array-as-prop': 'warn',
 		'no-inline-styles/no-inline-styles': 'error',
+		'react/jsx-sort-props': [
+			'warn',
+			{
+				callbacksLast: true,
+				shorthandFirst: true,
+				reservedFirst: true,
+			},
+		],
 
 		// eslint rules
 		'consistent-return': [
@@ -65,21 +80,22 @@ module.exports = {
 		'prefer-arrow-callback': ['warn', { allowNamedFunctions: true }],
 		'no-console': 'warn', // All consoles are automatically removed in production
 		'no-void': ['error', { allowAsStatement: true }],
+		'import/no-extraneous-dependencies': [
+			'error',
+			{ devDependencies: true },
+		],
+		camelcase: ['off'],
 
 		// import rules
-		// 'import/no-extraneous-dependencies': [
-		// 	'error',
-		// 	{ devDependencies: true },
-		// ],
-		// 'import/order': 'off',
-		// 'import/prefer-default-export': 'off',
-		// 'import/no-default-export': 'error',
+		'import/order': 'off',
+		'import/prefer-default-export': 'off',
+		'import/no-default-export': 'error',
 		'simple-import-sort/imports': 'warn',
 		'simple-import-sort/exports': 'warn',
-		// 'import/first': 'warn',
-		// 'import/newline-after-import': 'warn',
-		// 'import/no-duplicates': 'error',
-		// 'import/no-cycle': 'error',
+		'import/first': 'warn',
+		'import/newline-after-import': 'warn',
+		'import/no-duplicates': 'error',
+		'import/no-cycle': 'error',
 		'unused-imports/no-unused-imports': 'warn',
 		'no-unused-vars': 'off', // We use unused-imports/no-unused-vars instead
 		'@typescript-eslint/no-unused-vars': 'off', // We use unused-imports/no-unused-vars instead
@@ -93,11 +109,14 @@ module.exports = {
 		],
 
 		// typescript rules
+		'tsdoc/syntax': 'warn',
+		'@typescript-eslint/no-unnecessary-condition': 'off',
 		'@typescript-eslint/prefer-nullish-coalescing': [
 			'error',
 			{
-				ignoreConditionalTests: true,
-				ignorePrimitives: true,
+				ignorePrimitives: {
+					string: true,
+				},
 			},
 		],
 		'@typescript-eslint/no-explicit-any': [
@@ -128,40 +147,104 @@ module.exports = {
 			},
 		],
 		'@typescript-eslint/no-floating-promises': 'error',
+		// 'padding-line-between-statements': 'off',
+		'@typescript-eslint/naming-convention': [
+			'error',
+			{
+				selector: 'typeLike',
+				format: ['PascalCase', 'UPPER_CASE'],
+			},
+			{
+				selector: 'interface',
+				format: ['PascalCase', 'UPPER_CASE'],
+				custom: {
+					regex: '^I[A-Z]',
+					match: false,
+				},
+			},
+		],
 
-		// // unicorn
-		// // Common abbreviations are known and readable
-		// 'unicorn/prevent-abbreviations': 'off',
-		// 'unicorn/filename-case': [
-		// 	'error',
-		// 	{
-		// 		cases: {
-		// 			camelCase: true,
-		// 			pascalCase: true,
-		// 			kebabCase: true,
-		// 		},
-		// 		ignore: [
-		// 			// Ignore "CC" (Chaincode) anywhere in the filename
-		// 			'CC',
-		// 			// Ignore "CA" (Certificate Authority) anywhere in the filename
-		// 			'CA',
-		// 			// Ignore "VM" (Virtual Machine) anywhere in the filename
-		// 			'VM',
-		// 			// Ignore "SSH" anywhere in the filename
-		// 			'SSH',
-		// 			// Ignore files that start with numbers
-		// 			'^[0-9]',
-		// 		],
-		// 	},
-		// ],
-		// 'unicorn/no-null': 'off',
-		// 'unicorn/no-array-for-each': 'off',
-		// 'unicorn/no-array-reduce': 'off',
+		// unicorn
+		'unicorn/prevent-abbreviations': 'off', // Common abbreviations are known and readable
+		'unicorn/numeric-separators-style': 'warn',
+		'unicorn/filename-case': [
+			'error',
+			{
+				cases: {
+					camelCase: true,
+					pascalCase: true,
+					kebabCase: true,
+				},
+				ignore: [
+					// Ignore "CC" (Chaincode) anywhere in the filename
+					'CC',
+					// Ignore "CA" (Certificate Authority) anywhere in the filename
+					'CA',
+					// Ignore "VM" (Virtual Machine) anywhere in the filename
+					'VM',
+					// Ignore "SSH" anywhere in the filename
+					'SSH',
+					// Ignore files that start with numbers
+					'^[0-9]',
+				],
+			},
+		],
+		'unicorn/no-null': 'off',
+		'unicorn/no-array-for-each': 'off',
+		'unicorn/no-array-reduce': 'off',
 	},
 	ignorePatterns: [
 		// Ignore dotfiles
 		'.*.js',
 		'node_modules/',
 	],
-	overrides: [{ files: ['*.js?(x)', '*.ts?(x)'] }],
+	overrides: [
+		{ files: ['*.js?(x)', '*.ts?(x)'] },
+		{
+			files: [
+				'**/app/**/{page,loading,error,global-error,not-found,layout,template,default}.tsx',
+			],
+			rules: {
+				'import/no-default-export': 'off',
+				'react/function-component-definition': 'off',
+			},
+		},
+		{
+			// enable the rule specifically for TypeScriptJSX files
+			files: ['*.tsx', '*.ts'],
+			rules: {
+				'@typescript-eslint/explicit-function-return-type': 'off',
+			},
+		},
+		{
+			files: ['src/**/*', '__tests__/**/*'],
+			rules: {
+				'simple-import-sort/imports': [
+					'warn',
+					{
+						groups: [
+							// Node.js builtins. You could also generate this regex if you use a `.js` config.
+							// For example: `^(${require("module").builtinModules.join("|")})(/|$)`
+							[
+								'^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)',
+							],
+							// Packages. `react` related packages come first.
+							['^next', '^react$', '^@chakra', '^react', '^@?\\w'],
+							['^@repo'],
+							// Internal packages.
+							['^(@/)(/.*|$|..)'],
+							// Side effect imports.
+							['^\\u0000'],
+							// Parent imports. Put `..` last.
+							['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+							// Other relative imports. Put same-folder imports and `.` last.
+							['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+							// Style imports.
+							['@styles', '^.+\\.s?css$'],
+						],
+					},
+				],
+			},
+		},
+	],
 };
